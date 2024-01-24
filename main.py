5b1d929abcd91247eafe20e9f53fd548d121aa21
weakbox <connormcleod2000@icloud.com>
2024-01-23 20:41:53 -0400

    Added flickering to targets
    Targets now have the ability to flicker at a chosen rate

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ size = width, height = 1920, 1080
 screen = pg.display.set_mode(size)
 
 # Set the window caption
-pg.display.set_caption("GamBit Flicker Demo")
+pg.display.set_caption("GAMBiT: Flicker Demo")
 
 # Init the clock (sets the framerate)
 clock = pg.time.Clock()
@@ -23,12 +23,15 @@ clock = pg.time.Clock()
 pg.mixer.music.load("music/shell.mp3")
 pg.mixer.music.play(-1)
 
-# Super basic way to draw a square (target) at a given location
-target_1 = Target(screen, 127, 127, -1, -1)
-target_1.draw()
+# Print the title
+font = pg.font.Font(None, 64)
+text = font.render("GAMBiT: Flicker Demo", True, "white")
+textpos = text.get_rect(centerx = screen.get_width() / 2, y=10)
+screen.blit(text, textpos)
 
-target_2 = Target(screen, 255, 511, -1, -1)
-target_2.draw()
+# Super basic way to draw a square (target) at a given location
+target_1 = Target(screen, 127, 127, 20, -1)
+target_2 = Target(screen, 255, 511, 2, -1)
 
 while True:
     for event in pg.event.get():
@@ -38,7 +41,13 @@ while True:
             pg.quit()
             sys.exit()
 
-    pg.display.update()
+    screen.fill("black")
+
+    target_1.draw()
+    target_2.draw()
+    screen.blit(text, textpos)
+
+    pg.display.flip()
 
     # Limit the framerate to the integer specified
     clock.tick(144)
--- target.py
+++ target.py
@@ -3,7 +3,6 @@ import pygame as pg
 pg.init()
 
 class Target:
-    
     # Initialize target
     def __init__(self, screen, x_pos, y_pos, freq, focused):
         self.screen = screen
@@ -11,7 +10,12 @@ class Target:
         self.y_pos = y_pos
         self.freq = freq
         self.focused = focused
+        self.timer = 0
   
     # Draw target on screen at predetermined position
     def draw(self):
-        pg.draw.rect(self.screen, (255, 255, 255), (self.x_pos, self.y_pos, 64, 64))
+        if self.timer >= self.freq:
+            pg.draw.rect(self.screen, (255, 255, 255), (self.x_pos, self.y_pos, 64, 64))
+            self.timer = 0
+        self.timer += 1
+        
